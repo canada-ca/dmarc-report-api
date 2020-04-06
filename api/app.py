e124a1e94e04a577916d9d29dde98ae8ffe402e4
IdezHD <neatnick27@gmail.com>
2020-04-06 15:17:45 -0300

    Initialized API

---
+++ api/app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_graphql import GraphQLView
+from waitress import serve
+
+from backend.security_check import SecurityAnalysisBackend
+from schema import schema
+
+app = Flask(__name__)
+
+
+backend = SecurityAnalysisBackend(max_depth=20, max_cost=1000)
+
+
+app.add_url_rule(
+    '/graphql',
+    view_func=GraphQLView.as_view(
+        'graphql',
+        schema=schema,
+        backend=backend,
+        graphiql=True
+    )
+)
+
+if __name__ == '__main__':
+    serve(app, host='0.0.0.0', port=5000)
