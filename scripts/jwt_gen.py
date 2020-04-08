947e9ee3db966047af595494a70fcaa6b0e6032e
IdezHD <neatnick27@gmail.com>
2020-04-08 14:30:06 -0300

    Moved token gen into a function

--- scripts/jwt_gen.py
+++ scripts/jwt_gen.py
@@ -4,18 +4,20 @@ import os
 
 from graphql import GraphQLError
 
-try:
-    payload = {
-        'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0,seconds=1),
-        'iat': datetime.datetime.utcnow(),
-        'api_token': os.getenv('TOKEN')
-    }
-    token = jwt.encode(
-        payload,
-        str(os.getenv('JWT_SECRET')),
-        algorithm='HS256'
-    ).decode('utf-8')
-except Exception as e:
-    raise GraphQLError('Token Generation Error: ' + str(e))
 
-print(token)
+def jwt_gen(token):
+    try:
+        payload = {
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0,seconds=1),
+            'iat': datetime.datetime.utcnow(),
+            'api_token': token
+        }
+        token = jwt.encode(
+            payload,
+            str(os.getenv('JWT_SECRET')),
+            algorithm='HS256'
+        ).decode('utf-8')
+    except Exception as e:
+        raise GraphQLError('Token Generation Error: ' + str(e))
+
+    return token
