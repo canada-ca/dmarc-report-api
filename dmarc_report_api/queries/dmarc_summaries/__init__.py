0dd5a4c971fab964e6d4dbae7e7d7fa946a6397b
IdezHD <neatnick27@gmail.com>
2020-05-07 13:40:20 -0300

    Moved query object creation into __init__.py, all files have been formatted by black

--- dmarc_report_api/queries/dmarc_summaries/__init__.py
+++ dmarc_report_api/queries/dmarc_summaries/__init__.py
@@ -1,7 +1,49 @@
+import graphene
+
+from dmarc_report_api.scalars import URL
+
 from dmarc_report_api.queries.dmarc_summaries.summary_query import DmarcSummaries
 from dmarc_report_api.queries.dmarc_summaries.period_resolver import (
     resolve_dmarc_summary_by_period,
 )
 from dmarc_report_api.queries.dmarc_summaries.total_resolver import (
     resolve_total_dmarc_summaries,
+)
+
+get_dmarc_summary_by_period = graphene.Field(
+    lambda: DmarcSummaries,
+    domain=graphene.Argument(
+        URL, description="Domain used to select reports", required=True
+    ),
+    start_date=graphene.Argument(
+        graphene.String,
+        description="Set the start date in a date range select.",
+        required=False,
+    ),
+    end_date=graphene.Argument(
+        graphene.String,
+        description="Set the end date in a date range select.",
+        required=False,
+    ),
+    description="Summarized DMARC aggregate reports",
+    resolver=resolve_dmarc_summary_by_period,
+)
+
+get_total_dmarc_summaries = graphene.List(
+    lambda: DmarcSummaries,
+    domain=graphene.Argument(
+        URL, description="Domain used to select reports", required=True
+    ),
+    start_date=graphene.Argument(
+        graphene.String,
+        description="Set the start date in a date range select.",
+        required=False,
+    ),
+    end_date=graphene.Argument(
+        graphene.String,
+        description="Set the end date in a date range select.",
+        required=False,
+    ),
+    description="Summarized DMARC aggregate reports",
+    resolver=resolve_total_dmarc_summaries,
 )
--- dmarc_report_api/schema.py
+++ dmarc_report_api/schema.py
@@ -2,58 +2,20 @@ import graphene
 
 from graphene import relay
 
-from dmarc_report_api.scalars.url_scalar import URL
-from dmarc_report_api.queries import (
-    DmarcSummaries,
-    resolve_dmarc_summary_by_period,
-    resolve_total_dmarc_summaries,
+from dmarc_report_api.queries.dmarc_summaries import (
+    get_dmarc_summary_by_period,
+    get_total_dmarc_summaries,
 )
 
 
 class Query(graphene.ObjectType):
     node = relay.Node.Field()
 
-    get_dmarc_summary_by_period = graphene.Field(
-        lambda: DmarcSummaries,
-        domain=graphene.Argument(
-            URL, description="Domain used to select reports", required=True
-        ),
-        start_date=graphene.Argument(
-            graphene.String,
-            description="Set the start date in a date range select.",
-            required=False,
-        ),
-        end_date=graphene.Argument(
-            graphene.String,
-            description="Set the end date in a date range select.",
-            required=False,
-        ),
-        description="Summarized DMARC aggregate reports",
-    )
+    # Get dmarc summary query object
+    get_dmarc_summary_by_period = get_dmarc_summary_by_period
 
-    def resolve_get_dmarc_summary_by_period(self, info, **kwargs):
-        return resolve_dmarc_summary_by_period(self, info, **kwargs)
-
-    get_total_dmarc_summaries = graphene.List(
-        lambda: DmarcSummaries,
-        domain=graphene.Argument(
-            URL, description="Domain used to select reports", required=True
-        ),
-        start_date=graphene.Argument(
-            graphene.String,
-            description="Set the start date in a date range select.",
-            required=False,
-        ),
-        end_date=graphene.Argument(
-            graphene.String,
-            description="Set the end date in a date range select.",
-            required=False,
-        ),
-        description="Summarized DMARC aggregate reports",
-    )
-
-    def resolve_get_total_dmarc_summaries(self, info, **kwargs):
-        return resolve_total_dmarc_summaries(self, info, **kwargs)
+    # Get all dmarc summaries query object
+    get_total_dmarc_summaries = get_total_dmarc_summaries
 
 
 schema = graphene.Schema(query=Query)
