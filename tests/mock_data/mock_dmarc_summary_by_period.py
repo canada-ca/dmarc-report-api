3b054fc490cc3b9a1764280dce9338c6cd14e0f0
IdezHD <neatnick27@gmail.com>
2020-05-08 12:18:43 -0300

    created basic structures for total summaries, and period summary queries

---
+++ tests/mock_data/mock_dmarc_summary_by_period.py
@@ -0,0 +1,24 @@
+mock_data = [
+    {
+        "start_date": "2020-04-01",
+        "end_date": "2020-04-30",
+        "top_senders": {
+            "full_pass": [],
+            "spf_failure": [],
+            "spf_misaligned": [],
+            "dkim_failure": [],
+            "dkim_misaligned": [],
+            "dmarc_failure": []
+        },
+        "category_totals": {
+            "dmarc-fail-none": 0,
+            "spf-pass/dkim-fail": 0,
+            "spf-pass/dkim-pass": 0,
+            "dmarc-fail-reject": 0,
+            "spf-fail/dkim-pass": 0,
+            "arc-pass": 0,
+            "dmarc-fail-quarantine": 0,
+            "forwarded": 0
+        }
+    }
+]
---
+++ tests/mock_data/mock_total_dmarc_summaries.py
@@ -0,0 +1,311 @@
+mock_data = [
+    {
+        "start_date": "2020-04-08",
+        "end_date": "2020-05-08",
+        "top_senders": {
+            "full_pass": [],
+            "spf_failure": [],
+            "spf_misaligned": [],
+            "dkim_failure": [],
+            "dkim_misaligned": [],
+            "dmarc_failure": []
+        },
+        "category_totals": {
+            "dmarc-fail-none": 0,
+            "spf-pass/dkim-fail": 0,
+            "spf-pass/dkim-pass": 0,
+            "dmarc-fail-reject": 0,
+            "spf-fail/dkim-pass": 0,
+            "arc-pass": 0,
+            "dmarc-fail-quarantine": 0,
+            "forwarded": 0
+        }
+    },
+    {
+        "start_date": "2020-05-01",
+        "end_date": "2020-05-31",
+        "top_senders": {
+            "full_pass": [],
+            "spf_failure": [],
+            "spf_misaligned": [],
+            "dkim_failure": [],
+            "dkim_misaligned": [],
+            "dmarc_failure": []
+        },
+        "category_totals": {
+            "dmarc-fail-none": 0,
+            "spf-pass/dkim-fail": 0,
+            "spf-pass/dkim-pass": 0,
+            "dmarc-fail-reject": 0,
+            "spf-fail/dkim-pass": 0,
+            "arc-pass": 0,
+            "dmarc-fail-quarantine": 0,
+            "forwarded": 0
+        }
+    },
+    {
+        "start_date": "2020-04-01",
+        "end_date": "2020-04-30",
+        "top_senders": {
+            "full_pass": [],
+            "spf_failure": [],
+            "spf_misaligned": [],
+            "dkim_failure": [],
+            "dkim_misaligned": [],
+            "dmarc_failure": []
+        },
+        "category_totals": {
+            "dmarc-fail-none": 0,
+            "spf-pass/dkim-fail": 0,
+            "spf-pass/dkim-pass": 0,
+            "dmarc-fail-reject": 0,
+            "spf-fail/dkim-pass": 0,
+            "arc-pass": 0,
+            "dmarc-fail-quarantine": 0,
+            "forwarded": 0
+        }
+    },
+    {
+        "start_date": "2020-03-01",
+        "end_date": "2020-03-31",
+        "top_senders": {
+            "full_pass": [],
+            "spf_failure": [],
+            "spf_misaligned": [],
+            "dkim_failure": [],
+            "dkim_misaligned": [],
+            "dmarc_failure": []
+        },
+        "category_totals": {
+            "dmarc-fail-none": 0,
+            "spf-pass/dkim-fail": 0,
+            "spf-pass/dkim-pass": 0,
+            "dmarc-fail-reject": 0,
+            "spf-fail/dkim-pass": 0,
+            "arc-pass": 0,
+            "dmarc-fail-quarantine": 0,
+            "forwarded": 0
+        }
+    },
+    {
+        "start_date": "2020-02-01",
+        "end_date": "2020-02-29",
+        "top_senders": {
+            "full_pass": [],
+            "spf_failure": [],
+            "spf_misaligned": [],
+            "dkim_failure": [],
+            "dkim_misaligned": [],
+            "dmarc_failure": []
+        },
+        "category_totals": {
+            "dmarc-fail-none": 0,
+            "spf-pass/dkim-fail": 0,
+            "spf-pass/dkim-pass": 0,
+            "dmarc-fail-reject": 0,
+            "spf-fail/dkim-pass": 0,
+            "arc-pass": 0,
+            "dmarc-fail-quarantine": 0,
+            "forwarded": 0
+        }
+    },
+    {
+        "start_date": "2020-01-01",
+        "end_date": "2020-01-31",
+        "top_senders": {
+            "full_pass": [],
+            "spf_failure": [],
+            "spf_misaligned": [],
+            "dkim_failure": [],
+            "dkim_misaligned": [],
+            "dmarc_failure": []
+        },
+        "category_totals": {
+            "dmarc-fail-none": 0,
+            "spf-pass/dkim-fail": 0,
+            "spf-pass/dkim-pass": 0,
+            "dmarc-fail-reject": 0,
+            "spf-fail/dkim-pass": 0,
+            "arc-pass": 0,
+            "dmarc-fail-quarantine": 0,
+            "forwarded": 0
+        }
+    },
+    {
+        "start_date": "2019-12-01",
+        "end_date": "2019-12-31",
+        "top_senders": {
+            "full_pass": [],
+            "spf_failure": [],
+            "spf_misaligned": [],
+            "dkim_failure": [],
+            "dkim_misaligned": [],
+            "dmarc_failure": []
+        },
+        "category_totals": {
+            "dmarc-fail-none": 0,
+            "spf-pass/dkim-fail": 0,
+            "spf-pass/dkim-pass": 0,
+            "dmarc-fail-reject": 0,
+            "spf-fail/dkim-pass": 0,
+            "arc-pass": 0,
+            "dmarc-fail-quarantine": 0,
+            "forwarded": 0
+        }
+    },
+    {
+        "start_date": "2019-11-01",
+        "end_date": "2019-11-30",
+        "top_senders": {
+            "full_pass": [],
+            "spf_failure": [],
+            "spf_misaligned": [],
+            "dkim_failure": [],
+            "dkim_misaligned": [],
+            "dmarc_failure": []
+        },
+        "category_totals": {
+            "dmarc-fail-none": 0,
+            "spf-pass/dkim-fail": 0,
+            "spf-pass/dkim-pass": 0,
+            "dmarc-fail-reject": 0,
+            "spf-fail/dkim-pass": 0,
+            "arc-pass": 0,
+            "dmarc-fail-quarantine": 0,
+            "forwarded": 0
+        }
+    },
+    {
+        "start_date": "2019-10-01",
+        "end_date": "2019-10-31",
+        "top_senders": {
+            "full_pass": [],
+            "spf_failure": [],
+            "spf_misaligned": [],
+            "dkim_failure": [],
+            "dkim_misaligned": [],
+            "dmarc_failure": []
+        },
+        "category_totals": {
+            "dmarc-fail-none": 0,
+            "spf-pass/dkim-fail": 0,
+            "spf-pass/dkim-pass": 0,
+            "dmarc-fail-reject": 0,
+            "spf-fail/dkim-pass": 0,
+            "arc-pass": 0,
+            "dmarc-fail-quarantine": 0,
+            "forwarded": 0
+        }
+    },
+    {
+        "start_date": "2019-09-01",
+        "end_date": "2019-09-30",
+        "top_senders": {
+            "full_pass": [],
+            "spf_failure": [],
+            "spf_misaligned": [],
+            "dkim_failure": [],
+            "dkim_misaligned": [],
+            "dmarc_failure": []
+        },
+        "category_totals": {
+            "dmarc-fail-none": 0,
+            "spf-pass/dkim-fail": 0,
+            "spf-pass/dkim-pass": 0,
+            "dmarc-fail-reject": 0,
+            "spf-fail/dkim-pass": 0,
+            "arc-pass": 0,
+            "dmarc-fail-quarantine": 0,
+            "forwarded": 0
+        }
+    },
+    {
+        "start_date": "2019-08-01",
+        "end_date": "2019-08-31",
+        "top_senders": {
+            "full_pass": [],
+            "spf_failure": [],
+            "spf_misaligned": [],
+            "dkim_failure": [],
+            "dkim_misaligned": [],
+            "dmarc_failure": []
+        },
+        "category_totals": {
+            "dmarc-fail-none": 0,
+            "spf-pass/dkim-fail": 0,
+            "spf-pass/dkim-pass": 0,
+            "dmarc-fail-reject": 0,
+            "spf-fail/dkim-pass": 0,
+            "arc-pass": 0,
+            "dmarc-fail-quarantine": 0,
+            "forwarded": 0
+        }
+    },
+    {
+        "start_date": "2019-07-01",
+        "end_date": "2019-07-31",
+        "top_senders": {
+            "full_pass": [],
+            "spf_failure": [],
+            "spf_misaligned": [],
+            "dkim_failure": [],
+            "dkim_misaligned": [],
+            "dmarc_failure": []
+        },
+        "category_totals": {
+            "dmarc-fail-none": 0,
+            "spf-pass/dkim-fail": 0,
+            "spf-pass/dkim-pass": 0,
+            "dmarc-fail-reject": 0,
+            "spf-fail/dkim-pass": 0,
+            "arc-pass": 0,
+            "dmarc-fail-quarantine": 0,
+            "forwarded": 0
+        }
+    },
+    {
+        "start_date": "2019-06-01",
+        "end_date": "2019-06-30",
+        "top_senders": {
+            "full_pass": [],
+            "spf_failure": [],
+            "spf_misaligned": [],
+            "dkim_failure": [],
+            "dkim_misaligned": [],
+            "dmarc_failure": []
+        },
+        "category_totals": {
+            "dmarc-fail-none": 0,
+            "spf-pass/dkim-fail": 0,
+            "spf-pass/dkim-pass": 0,
+            "dmarc-fail-reject": 0,
+            "spf-fail/dkim-pass": 0,
+            "arc-pass": 0,
+            "dmarc-fail-quarantine": 0,
+            "forwarded": 0
+        }
+    },
+    {
+        "start_date": "2019-05-01",
+        "end_date": "2019-05-31",
+        "top_senders": {
+            "full_pass": [],
+            "spf_failure": [],
+            "spf_misaligned": [],
+            "dkim_failure": [],
+            "dkim_misaligned": [],
+            "dmarc_failure": []
+        },
+        "category_totals": {
+            "dmarc-fail-none": 0,
+            "spf-pass/dkim-fail": 0,
+            "spf-pass/dkim-pass": 0,
+            "dmarc-fail-reject": 0,
+            "spf-fail/dkim-pass": 0,
+            "arc-pass": 0,
+            "dmarc-fail-quarantine": 0,
+            "forwarded": 0
+        }
+    }
+]
+
