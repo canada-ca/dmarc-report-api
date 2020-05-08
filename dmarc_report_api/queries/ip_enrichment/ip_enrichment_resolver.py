bd7b87df99a9235410bf217e78551a7cd88cd3da
IdezHD <neatnick27@gmail.com>
2020-05-08 09:50:28 -0300

    Initial test for ip enrichment query

--- dmarc_report_api/queries/ip_enrichment/ip_enrichment_resolver.py
+++ dmarc_report_api/queries/ip_enrichment/ip_enrichment_resolver.py
@@ -1,13 +1,11 @@
 from graphql import GraphQLError
 
-import json
-
 from dmarc_report_api.queries.ip_enrichment import IPEnrichment
 from dmarc_report_api.shared_functions import cleanse_input
 from dmarc_report_api.data.fetch_ip_enrichment import fetch_ip_enrichment
 from dmarc_report_api.auth import require_token
 
-
+# @require_token
 def resolve_ip_enrichment(self, info, **kwargs):
     domain = cleanse_input(kwargs.get("domain", None))
 
--- dmarc_report_api/schema.py
+++ dmarc_report_api/schema.py
@@ -1,7 +1,5 @@
 import graphene
 
-from graphene import relay
-
 from dmarc_report_api.queries.dmarc_summaries import (
     get_dmarc_summary_by_period,
     get_total_dmarc_summaries,
@@ -13,8 +11,6 @@ from dmarc_report_api.queries.ip_enrichment import (
 
 
 class Query(graphene.ObjectType):
-    # node = relay.Node.Field()
-
     # Get dmarc summary query object
     get_dmarc_summary_by_period = get_dmarc_summary_by_period
 
