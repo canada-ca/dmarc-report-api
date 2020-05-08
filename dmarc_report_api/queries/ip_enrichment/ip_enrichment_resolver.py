d32f72809a3a5321696c381beb234e01680efbb6
IdezHD <neatnick27@gmail.com>
2020-05-08 11:02:50 -0300

    Completed initial ip enrichment query

--- dmarc_report_api/queries/ip_enrichment/ip_enrichment_resolver.py
+++ dmarc_report_api/queries/ip_enrichment/ip_enrichment_resolver.py
@@ -5,7 +5,7 @@ from dmarc_report_api.shared_functions import cleanse_input
 from dmarc_report_api.data.fetch_ip_enrichment import fetch_ip_enrichment
 from dmarc_report_api.auth import require_token
 
-# @require_token
+@require_token
 def resolve_ip_enrichment(self, info, **kwargs):
     domain = cleanse_input(kwargs.get("domain", None))
 
--- tests/dmarc_report_api/test_ip_enrichment_query.py
+++ tests/dmarc_report_api/test_ip_enrichment_query.py
@@ -1,21 +1,33 @@
-import unittest
+import os
 import pytest
 import pytest_mock
 
+from flask import Request
 from graphene.test import Client
+from pytest import fail
+from werkzeug.test import create_environ
 
-from tests.mock_data.mock_ip_enrichment import mock_data
 from dmarc_report_api.schema import schema
+from scripts.jwt_gen import jwt_gen
+from tests.mock_data.mock_ip_enrichment import mock_data
 
 
-def test(mocker):
+def auth_header():
+    env = create_environ()
+    env.update(HTTP_AUTHORIZATION=jwt_gen(os.getenv('TOKEN')))
+    return Request(env)
+
+
+def test_query_get_ip_enrichment(mocker):
+    """
+    Test to see if the getIpEnrichment query works
+    """
     mocker.patch(
-        'dmarc_report_api.queries.ip_enrichment.ip_enrichment_resolver.fetch_ip_enrichment',
+        'dmarc_report_api.queries.ip_enrichment.ip_enrichment_resolver'
+        '.fetch_ip_enrichment',
         autospec=True,
         return_value=mock_data
     )
-
-    client = Client(schema=schema)
 
     query = '''
     {
@@ -38,7 +50,10 @@ def test(mocker):
     }
     '''
 
-    executed = client.execute(query)
+    executed = Client(schema=schema).execute(
+        query,
+        context_value=auth_header()
+    )
 
     expected_result = {
         'data': {
@@ -60,5 +75,13 @@ def test(mocker):
             ]
         }
     }
+
+    if "errors" in executed:
+        fail(
+            "Tried to execute the getIpEnrichment query, this error occurred: "
+            "{}".format(
+                executed
+            )
+        )
 
     assert executed == expected_result
