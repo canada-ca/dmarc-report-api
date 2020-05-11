0c9bb3659d4d30da18e25a991921c11cd2d3ec09
IdezHD <neatnick27@gmail.com>
2020-05-11 07:11:57 -0300

    Completed error date tests for get_summary_by_period query

--- tests/dmarc_report_api/test_get_summary_by_period.py
+++ tests/dmarc_report_api/test_get_summary_by_period.py
@@ -11,7 +11,8 @@ from werkzeug.test import create_environ
 from dmarc_report_api.schema import schema
 from scripts.jwt_gen import jwt_gen
 from tests.mock_data.mock_dmarc_summary_by_period import mock_data
-from tests.expected_result_dictionaries.summary_data_by_period import expected_summary_period_data
+from tests.expected_result_dictionaries.summary_data_by_period import \
+    expected_summary_period_data
 
 
 def auth_header():
@@ -101,8 +102,194 @@ def test_valid_query_get_dmarc_summary_by_period(mocker):
 
     if "errors" in executed:
         fail(
-            "Tried to execute the getDmarcSummaryByPeriod query, Instead: {}"\
+            "Tried to execute the getDmarcSummaryByPeriod query, Instead: {}"
             .format(dumps(executed, indent=2))
         )
 
     assert executed == expected_summary_period_data
+
+
+def test_invalid_query_get_dmarc_summary_by_period_incorrect_start_date(mocker):
+    """
+    Test to see if correct error appears when start date does not match any
+    periods in the db
+    """
+    mocker.patch(
+        "dmarc_report_api.queries.dmarc_summaries.period_resolver.fetch_summary_by_period",
+        autospec=True,
+        return_value=[]
+    )
+
+    query = '''
+        {
+            getDmarcSummaryByPeriod (
+                domain: "test.domain.ca"
+                startDate: "2020-04-05"
+                endDate: "2020-04-30"
+            ) {
+                startDate
+                endDate
+                topSenders {
+                    fullPass {
+                        sourceIpAddress
+                        spfDomains
+                        dkimDomains
+                        dkimSelectors
+                        total
+                    }
+                    spfFailure {
+                        sourceIpAddress
+                        spfDomains
+                        dkimDomains
+                        dkimSelectors
+                        total
+                    }
+                    spfMisaligned {
+                        sourceIpAddress
+                        spfDomains
+                        dkimDomains
+                        dkimSelectors
+                        total
+                    }
+                    dkimFailure {
+                        sourceIpAddress
+                        spfDomains
+                        dkimDomains
+                        dkimSelectors
+                        total
+                    }
+                    dkimMisaligned {
+                        sourceIpAddress
+                        spfDomains
+                        dkimDomains
+                        dkimSelectors
+                        total
+                    }
+                    dmarcFailure {
+                        sourceIpAddress
+                        spfDomains
+                        dkimDomains
+                        dkimSelectors
+                        total
+                    }
+                }
+                categoryTotals {
+                    spfPassDkimPass
+                    spfFailDkimPass
+                    dmarcFailNone
+                    forwarded
+                    arcPass
+                    spfPassDkimFail
+                    dmarcFailReject
+                    dmarcFailQuarantine
+                }
+            }
+        }
+        '''
+
+    executed = Client(schema=schema).execute(query, context_value=auth_header())
+
+    if "errors" not in executed:
+        fail(
+            "Tried to execute the getDmarcSummaryByPeriod query thinking it "
+            "would error out, Instead: {}".format(dumps(executed, indent=2))
+        )
+
+    errors, data = executed.values()
+    [first] = errors
+    expected_message = first["message"]
+    assert expected_message == "Error, there is no data for that time " \
+                               "period, or domain is incorrect"
+
+
+def test_invalid_query_get_dmarc_summary_by_period_incorrect_end_date(mocker):
+    """
+    Test to see if correct error appears when end date does not match any
+    periods in the db
+    """
+    mocker.patch(
+        "dmarc_report_api.queries.dmarc_summaries.period_resolver.fetch_summary_by_period",
+        autospec=True,
+        return_value=[]
+    )
+
+    query = '''
+        {
+            getDmarcSummaryByPeriod (
+                domain: "test.domain.ca"
+                startDate: "2020-04-01"
+                endDate: "2020-04-25"
+            ) {
+                startDate
+                endDate
+                topSenders {
+                    fullPass {
+                        sourceIpAddress
+                        spfDomains
+                        dkimDomains
+                        dkimSelectors
+                        total
+                    }
+                    spfFailure {
+                        sourceIpAddress
+                        spfDomains
+                        dkimDomains
+                        dkimSelectors
+                        total
+                    }
+                    spfMisaligned {
+                        sourceIpAddress
+                        spfDomains
+                        dkimDomains
+                        dkimSelectors
+                        total
+                    }
+                    dkimFailure {
+                        sourceIpAddress
+                        spfDomains
+                        dkimDomains
+                        dkimSelectors
+                        total
+                    }
+                    dkimMisaligned {
+                        sourceIpAddress
+                        spfDomains
+                        dkimDomains
+                        dkimSelectors
+                        total
+                    }
+                    dmarcFailure {
+                        sourceIpAddress
+                        spfDomains
+                        dkimDomains
+                        dkimSelectors
+                        total
+                    }
+                }
+                categoryTotals {
+                    spfPassDkimPass
+                    spfFailDkimPass
+                    dmarcFailNone
+                    forwarded
+                    arcPass
+                    spfPassDkimFail
+                    dmarcFailReject
+                    dmarcFailQuarantine
+                }
+            }
+        }
+        '''
+
+    executed = Client(schema=schema).execute(query, context_value=auth_header())
+
+    if "errors" not in executed:
+        fail(
+            "Tried to execute the getDmarcSummaryByPeriod query thinking it "
+            "would error out, Instead: {}".format(dumps(executed, indent=2))
+        )
+
+    errors, data = executed.values()
+    [first] = errors
+    expected_message = first["message"]
+    assert expected_message == "Error, there is no data for that time " \
+                               "period, or domain is incorrect"
