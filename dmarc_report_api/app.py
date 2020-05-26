7b52496c660a7ad0a648151bb84ffd0ded850da3
IdezHD <neatnick27@gmail.com>
2020-05-26 08:41:03 -0300

    Use custom port, and host info

--- dmarc_report_api/app.py
+++ dmarc_report_api/app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_graphql import GraphQLView
 from waitress import serve
@@ -5,11 +7,12 @@ from waitress import serve
 from dmarc_report_api.backend.security_check import SecurityAnalysisBackend
 from dmarc_report_api.schema import schema
 
+HOST = os.getenv("HOST", "localhost")
+PORT = os.getenv("PORT", 8080)
+
 app = Flask(__name__)
 
-
 backend = SecurityAnalysisBackend(max_depth=20, max_cost=1000)
-
 
 app.add_url_rule(
     "/graphql",
@@ -19,4 +22,4 @@ app.add_url_rule(
 )
 
 if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=5000)
+    serve(app, host=HOST, port=PORT)
